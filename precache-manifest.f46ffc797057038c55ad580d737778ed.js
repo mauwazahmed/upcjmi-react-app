2f78dc6871dbe3f17adddb3999d8b6bf7ba172aa
AHMAD HASSAN ANSARI <alhaqhassan@gmail.com>
2019-09-16 16:01:00 +0530

    Updates

---
+++ precache-manifest.f46ffc797057038c55ad580d737778ed.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32203d897239959e1c9a3263fbb6e0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98c0fd3d1ade43794941",
+    "url": "/static/css/0.ce82722d.chunk.css"
+  },
+  {
+    "revision": "dd99734106b6bf86b07c",
+    "url": "/static/css/1.2931bd5b.chunk.css"
+  },
+  {
+    "revision": "197fdc33156e80ea92ae",
+    "url": "/static/css/2.ae50f7ed.chunk.css"
+  },
+  {
+    "revision": "2ddd47201b40a44a43ab",
+    "url": "/static/css/5.ebea4668.chunk.css"
+  },
+  {
+    "revision": "47452e93348e650e5981",
+    "url": "/static/css/7.e2928d49.chunk.css"
+  },
+  {
+    "revision": "e590baa81820e2e39a45",
+    "url": "/static/css/8.adb33693.chunk.css"
+  },
+  {
+    "revision": "af4325fecc12d656ee08",
+    "url": "/static/css/main.dda41e6d.chunk.css"
+  },
+  {
+    "revision": "98c0fd3d1ade43794941",
+    "url": "/static/js/0.91e0cd81.chunk.js"
+  },
+  {
+    "revision": "dd99734106b6bf86b07c",
+    "url": "/static/js/1.aafb7a8c.chunk.js"
+  },
+  {
+    "revision": "1f1e02f5014beb598d65",
+    "url": "/static/js/10.b4589642.chunk.js"
+  },
+  {
+    "revision": "67c37a351b2dbc8e7733",
+    "url": "/static/js/11.498e5db9.chunk.js"
+  },
+  {
+    "revision": "b2b28206ba5dbbe0410b",
+    "url": "/static/js/12.89b1e9dd.chunk.js"
+  },
+  {
+    "revision": "f3038cf97f5e0de690e3",
+    "url": "/static/js/13.372d8475.chunk.js"
+  },
+  {
+    "revision": "e96cf9c4f007584bbc57",
+    "url": "/static/js/14.dd6be2d9.chunk.js"
+  },
+  {
+    "revision": "7dd696f1feb4918d7a24",
+    "url": "/static/js/15.b9ddb11d.chunk.js"
+  },
+  {
+    "revision": "2712e8a78b0b0d7a0e24",
+    "url": "/static/js/16.c51ac70b.chunk.js"
+  },
+  {
+    "revision": "3e7bae424070e1491315",
+    "url": "/static/js/17.734059e9.chunk.js"
+  },
+  {
+    "revision": "903c0c43acc1aaf5bd90",
+    "url": "/static/js/18.f0e28eca.chunk.js"
+  },
+  {
+    "revision": "90521d0ec26c9fa44f3d",
+    "url": "/static/js/19.cfdd93d6.chunk.js"
+  },
+  {
+    "revision": "197fdc33156e80ea92ae",
+    "url": "/static/js/2.df27460d.chunk.js"
+  },
+  {
+    "revision": "2ddd47201b40a44a43ab",
+    "url": "/static/js/5.0a8770b7.chunk.js"
+  },
+  {
+    "revision": "ea80413c9b2bc8d19afc",
+    "url": "/static/js/6.4c10027f.chunk.js"
+  },
+  {
+    "revision": "47452e93348e650e5981",
+    "url": "/static/js/7.2a5cb6cf.chunk.js"
+  },
+  {
+    "revision": "e590baa81820e2e39a45",
+    "url": "/static/js/8.2b153776.chunk.js"
+  },
+  {
+    "revision": "6e95f3068f0044e211ec",
+    "url": "/static/js/9.7180e53a.chunk.js"
+  },
+  {
+    "revision": "af4325fecc12d656ee08",
+    "url": "/static/js/main.50521faf.chunk.js"
+  },
+  {
+    "revision": "4146a1c41aee65072b69",
+    "url": "/static/js/runtime~main.07f6b7ca.js"
+  },
+  {
+    "revision": "ad13e81d8d147ad52b73f40382da5de7",
+    "url": "/static/media/Outlook100university.ad13e81d.png"
+  },
+  {
+    "revision": "8f7d9d8fe0c1c0a9484f6a79ca4f6284",
+    "url": "/static/media/Outlookrank.8f7d9d8f.png"
+  },
+  {
+    "revision": "e78b887b6ff90c6a1b5ade6886c06f23",
+    "url": "/static/media/QSWorldRank.e78b887b.png"
+  },
+  {
+    "revision": "b5a0c099f39de1c267a396c71cfaf7dc",
+    "url": "/static/media/RURUniversityRanking.b5a0c099.png"
+  },
+  {
+    "revision": "5d33d9419233688eeebae7c1a74d74c3",
+    "url": "/static/media/emphasiso-team.5d33d941.jpg"
+  },
+  {
+    "revision": "1b80d421a96f03b153b3c18338fe899d",
+    "url": "/static/media/jamia-logo.1b80d421.svg"
+  },
+  {
+    "revision": "9b0d72e6176f48fc5dd0b180660c4a35",
+    "url": "/static/media/placement-team.9b0d72e6.jpg"
+  },
+  {
+    "revision": "be176c8934c446adbe208e4ee7f75306",
+    "url": "/static/media/rihan-khan-suri-tpo-jamia-miilia-islamia.be176c89.png"
+  },
+  {
+    "revision": "bee290f41466ff582ff55a7c1adc2ee8",
+    "url": "/static/media/tpo-receving-award.bee290f4.jpg"
+  },
+  {
+    "revision": "fc6aac216187ab8a335459e4f6c8a510",
+    "url": "/static/media/tpo.fc6aac21.jpg"
+  },
+  {
+    "revision": "ad0e35ff7d6c8812af988d410eb24784",
+    "url": "/static/media/vc-jamia-millia-islamia.ad0e35ff.png"
+  },
+  {
+    "revision": "db56fcce4850292529c4882a8aa3e309",
+    "url": "/static/media/vc-with-pm.db56fcce.jpg"
+  },
+  {
+    "revision": "4baa1b05a2b1b749f73a7b8a6a080437",
+    "url": "/static/media/vc-with-president.4baa1b05.jpg"
+  },
+  {
+    "revision": "9b42858046901a7b09d200db417523dd",
+    "url": "/static/media/zainul-abdin-jaffery.9b428580.png"
+  }
+]);
